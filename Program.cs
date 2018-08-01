c41827ba3da2feca745c979feb53d6bc7f8c7909
Sebastián Iñones <sebainones@gmail.com>
2018-08-01 13:34:10 +0200

    Initial commit of a very simple .Net Core Console App for learning purposes.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DotNetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sebas = "sebastian";
+            Console.WriteLine($"Hello {sebas}!");
+        }
+    }
+}
